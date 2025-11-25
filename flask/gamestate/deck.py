2f59a7e849be12c01a0c2baaea4c39bac6e7987d
Matthieu Sarter <matthieu@sarter.fr>
2025-11-25 23:14:55 +0100

    feat(flask): add "non-vote" cards (question mark and coffee break)

--- flask/gamestate/deck.py
+++ flask/gamestate/deck.py
@@ -3,8 +3,8 @@ from enum import Enum
 
 class Deck(Enum):
     """List of the decks of cards available"""
-    FIBONACCI = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    MODIFIED_FIBONACCI = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100]
-    POWERS = [0, 1, 2, 4, 8, 16, 32, 64]
-    TRUST_VOTE = [0, 1, 2, 3, 4, 5]
-    T_SHIRTS = [1, 2, 3, 4, 5, 6, 7]  # Each value maps to a t-shirt size from XXS to XXL
+    FIBONACCI = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, -1, -2]
+    MODIFIED_FIBONACCI = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100, -1, -2]
+    POWERS = [0, 1, 2, 4, 8, 16, 32, 64, -1, -2]
+    TRUST_VOTE = [0, 1, 2, 3, 4, 5, -1, -2]
+    T_SHIRTS = [1, 2, 3, 4, 5, 6, 7, -1, -2]  # Each value maps to a t-shirt size from XXS to XXL
